6483a907099b82087ca0e3b577faefd8c14d9493
Henrique Custódio <henriqueffc17@gmail.com>
2022-03-10 17:36:02 -0300

    Update 4-zsh-ohmyzsh.sh

--- 4-zsh-ohmyzsh.sh
+++ 4-zsh-ohmyzsh.sh
@@ -10,11 +10,11 @@ FIM='\e[0m'
 
 if ls -lha ~ | grep -oq .zshrc;
 then
-	echo "${AZUL}Arquivo .zshrc encontrado${FIM}"
-	echo "${AZUL}Instalando Oh my Zsh, plugins e Powerlevel10K${FIM}"
-	echo "${AZUL}Começando em 3...${FIM}" && sleep 1
-	echo "${AZUL}Começando em 2...${FIM}" && sleep 1
-	echo "${AZUL}Começando em 1...${FIM}" && sleep 1
+	echo -e "${AZUL}Arquivo .zshrc encontrado${FIM}"
+	echo -e "${AZUL}Instalando Oh my Zsh, plugins e Powerlevel10K${FIM}"
+	echo -e "${AZUL}Começando em 3...${FIM}" && sleep 1
+	echo -e "${AZUL}Começando em 2...${FIM}" && sleep 1
+	echo -e "${AZUL}Começando em 1...${FIM}" && sleep 1
 	sh -c "$(curl -fsSL https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
 	git clone https://github.com/zsh-users/zsh-syntax-highlighting.git ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
 	git clone https://github.com/zsh-users/zsh-autosuggestions ${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
@@ -27,9 +27,9 @@ then
 	sed -i "${linenumber}i fpath+=${ZSH_CUSTOM:-${ZSH:-~/.oh-my-zsh}/custom}/plugins/zsh-completions/src" ~/.zshrc
 	echo 'source ~/.bash_aliases' >> ~/.zshrc
 	git clone --depth=1 https://github.com/romkatv/powerlevel10k.git ${ZSH_CUSTOM:-$HOME/.oh-my-zsh/custom}/themes/powerlevel10k
-	echo "${AZUL}Instalação concluida. Reinicie o terminal para configurar o Powerlevel10K.${FIM}"
+	echo -e "${AZUL}Instalação concluida. Reinicie o terminal para configurar o Powerlevel10K.${FIM}"
 else
-	echo "${AZUL}Arquivo .zshrc não encontrado na pasta home. Leia as intruções de instalação. Fim${FIM}"
+	echo -e "${AZUL}Arquivo .zshrc não encontrado na pasta home. Leia as intruções de instalação. Fim${FIM}"
 fi 
 
 
