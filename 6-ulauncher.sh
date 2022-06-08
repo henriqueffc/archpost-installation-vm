31816a710921dd58676d06665d125c188f55e14a
Henrique Custódio <henriqueffc17@gmail.com>
2022-06-08 17:44:09 -0300

    update settings

--- 6-ulauncher.sh
+++ 6-ulauncher.sh
@@ -33,7 +33,6 @@ git clone https://github.com/dracula/ulauncher.git ~/.config/ulauncher/user-them
 git clone https://github.com/tom-james-watson/ulauncher-popdark.git ~/.config/ulauncher/user-themes/ulauncher-popdark
 
 #Backup das configurações 
-mv ~/.config/ulauncher/shortcuts.json ~/.config/ulauncher/shortcuts.json.bak
 cp ~/.config/ulauncher/settings.json ~/.config/ulauncher/settings.json.bak
 
 #Movendo o arquivo das configurações dos atalhos
@@ -46,12 +45,12 @@ sed -i 's/light/\dracula/' ~/.config/ulauncher/settings.json
 #sed -i 's/<Primary>space/\<Super>backslash/' ~/.config/ulauncher/settings.json
 
 #Instalando as extenções
-git clone https://github.com/leinardi/ulauncher-exit-gnome ~/.local/share/ulauncher/extensions
-git clone https://github.com/dalanicolai/gnome-tracker-extension ~/.local/share/ulauncher/extensions
-git clone https://github.com/fisadev/ulauncher-better-file-browser ~/.local/share/ulauncher/extensions
-git clone https://github.com/Doekeb/ulauncher-gnome-calculator ~/.local/share/ulauncher/extensions
-git clone https://github.com/isacikgoz/ukill ~/.local/share/ulauncher/extensions
-git clone https://ext.ulauncher.io/-/github-friday-ulauncher-clipboard ~/.local/share/ulauncher/extensions
-git clone https://ext.ulauncher.io/-/github-kuenzelit-ulauncher-firefox-bookmarks ~/.local/share/ulauncher/extensions
+git clone https://github.com/leinardi/ulauncher-exit-gnome ~/.local/share/ulauncher/extensions/ulauncher-exit-gnome
+git clone https://github.com/dalanicolai/gnome-tracker-extension ~/.local/share/ulauncher/extensions/gnome-tracker-extension
+git clone https://github.com/fisadev/ulauncher-better-file-browser ~/.local/share/ulauncher/extensions/ulauncher-better-file-browser
+git clone https://github.com/Doekeb/ulauncher-gnome-calculator ~/.local/share/ulauncher/extensions/ulauncher-gnome-calculator
+git clone https://github.com/isacikgoz/ukill ~/.local/share/ulauncher/extensions/ukill
+git clone https://ext.ulauncher.io/-/github-friday-ulauncher-clipboard ~/.local/share/ulauncher/extensions/github-friday-ulauncher-clipboard
+git clone https://ext.ulauncher.io/-/github-kuenzelit-ulauncher-firefox-bookmarks ~/.local/share/ulauncher/extensions/github-kuenzelit-ulauncher-firefox-bookmarks
 
 printf "%s $VERDE Fim! O atalho para o Ulauncher foi defino no script 2-pacote.sh (Super + \). REINICIE o computador para que as mudanças sejam aplicadas no Ulauncher $FIM \n"
