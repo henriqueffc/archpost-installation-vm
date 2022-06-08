615e55869d46abf978206dbf83980f58f96836f3
Henrique Custódio <henriqueffc17@gmail.com>
2022-06-08 17:34:46 -0300

    update

--- 6-ulauncher.sh
+++ 6-ulauncher.sh
@@ -23,14 +23,7 @@ echo -e "$AZUL
 -------------------------------------------------------------------------
         	Iniciando as configurações do Ulauncher
 -------------------------------------------------------------------------
-$FIM" && sleep 3
-
-#Backup das configurações 
-mv ~/.config/ulauncher/shortcuts.json ~/.config/ulauncher/shortcuts.json.bak
-cp ~/.config/ulauncher/settings.json ~/.config/ulauncher/settings.json.bak
-
-#Movendo o arquivo das configurações dos atalhos
-mv ./ulauncher/shortcuts.json ~/.config/ulauncher/
+$FIM" && sleep 8
 
 #Copiando os ícones
 tar -Jxxvf ./ulauncher/imagens.tar.xz -C ~/Imagens
@@ -39,6 +32,13 @@ tar -Jxxvf ./ulauncher/imagens.tar.xz -C ~/Imagens
 git clone https://github.com/dracula/ulauncher.git ~/.config/ulauncher/user-themes/dracula-ulauncher
 git clone https://github.com/tom-james-watson/ulauncher-popdark.git ~/.config/ulauncher/user-themes/ulauncher-popdark
 
+#Backup das configurações 
+mv ~/.config/ulauncher/shortcuts.json ~/.config/ulauncher/shortcuts.json.bak
+cp ~/.config/ulauncher/settings.json ~/.config/ulauncher/settings.json.bak
+
+#Movendo o arquivo das configurações dos atalhos
+mv ./ulauncher/shortcuts.json ~/.config/ulauncher/
+
 #Substituindo o tema
 sed -i 's/light/\dracula/' ~/.config/ulauncher/settings.json
 
@@ -46,7 +46,6 @@ sed -i 's/light/\dracula/' ~/.config/ulauncher/settings.json
 #sed -i 's/<Primary>space/\<Super>backslash/' ~/.config/ulauncher/settings.json
 
 #Instalando as extenções
-mkdir $HOME/.local/share/ulauncher/extensions
 git clone https://github.com/leinardi/ulauncher-exit-gnome ~/.local/share/ulauncher/extensions
 git clone https://github.com/dalanicolai/gnome-tracker-extension ~/.local/share/ulauncher/extensions
 git clone https://github.com/fisadev/ulauncher-better-file-browser ~/.local/share/ulauncher/extensions
@@ -55,4 +54,4 @@ git clone https://github.com/isacikgoz/ukill ~/.local/share/ulauncher/extensions
 git clone https://ext.ulauncher.io/-/github-friday-ulauncher-clipboard ~/.local/share/ulauncher/extensions
 git clone https://ext.ulauncher.io/-/github-kuenzelit-ulauncher-firefox-bookmarks ~/.local/share/ulauncher/extensions
 
-printf "%s $VERDE Fim! O atalho para o Ulauncher foi defino no script 2-pacote.sh (Super + \). REINICIE o computador para que as mudanças sejam aplicadas no Ulauncher $FIM \n" && sleep 2
+printf "%s $VERDE Fim! O atalho para o Ulauncher foi defino no script 2-pacote.sh (Super + \). REINICIE o computador para que as mudanças sejam aplicadas no Ulauncher $FIM \n"
